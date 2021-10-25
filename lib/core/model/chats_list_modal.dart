b038595d5226a36ef68b965155d981b96282d66e
khansawidhani <khansa.ashraf23@gmail.com>
2021-10-25 01:22:01 -0700

    Chats List Modal deleted

--- lib/core/model/chats_list_modal.dart
+++
@@ -1,8 +0,0 @@
-class ChatUsers{
-  String name;
-  String messageText;
-  String imgUrl;
-  String time;
-
-  ChatUsers({required this.name, required this.messageText, required this.imgUrl, required this.time});
-}
