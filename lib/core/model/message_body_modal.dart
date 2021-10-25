27963983c1c7eac3d9eb3ce7112c0e59a2d8a31c
khansawidhani <khansa.ashraf23@gmail.com>
2021-10-25 01:22:52 -0700

    Message Body Modal deleted

--- lib/core/model/message_body_modal.dart
+++
@@ -1,8 +0,0 @@
-class MessageBody{
-  MessageBody({required this.id, required this.messageContent, required this.messageType, required this.messageTime, required this.isRead});
-  String id;
-  String messageContent;
-  String messageType;
-  String messageTime;
-  bool isRead;
-}
