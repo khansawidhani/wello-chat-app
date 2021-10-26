0aff0ed5f8c6cfe236d203532288e38b0d937518
khansawidhani <khansa.ashraf23@gmail.com>
2021-10-25 23:42:30 -0700

    Minor update related to UI and styling

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:wello_chat_application/core/modal/message_body_modal.dart';
+import 'package:wello_chat_application/core/widgets/message_field.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -223,63 +224,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 itemCount: messages.length,
               ),
             ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                height: 52,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    
-                    ),
-                padding: EdgeInsets.only(
-                  left: 10,
-                  top: 10,
-                  bottom: 10,
-                ),
-                child: Row(
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.teal,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15.0))),
-                        width: 30,
-                        height: 30,
-                        child: Icon(Icons.add),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Expanded(
-                      child: TextField(
-                        minLines: 1,
-                        maxLines: 3,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Your Message..",
-                            hintStyle: TextStyle(
-                                fontStyle: FontStyle.italic,
-                                color: Colors.grey)),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    FloatingActionButton(
-                      onPressed: () {},
-                      child: Icon(
-                        Icons.send,
-                      ),
-                      elevation: 0.0,
-                    )
-                  ],
-                ),
-              ),
-            )
+            messageField()
           ],
         )
         );
