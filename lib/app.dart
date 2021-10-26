5ea5b5f83974e6b525ab8ccc87996f4f8ebf2554
khansawidhani <khansa.ashraf23@gmail.com>
2021-10-25 23:45:20 -0700

    Changes in Ui

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,14 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:wello_chat_application/core/theme.dart';
 import 'package:wello_chat_application/screens/chat.dart';
 import 'package:wello_chat_application/screens/home.dart';
+import 'package:wello_chat_application/screens/new_chat.dart';
+import 'package:wello_chat_application/screens/profile.dart';
 
 class App extends StatelessWidget {
-  App({ Key? key }) : super(key: key);
+  const App({ Key? key }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +17,9 @@ class App extends StatelessWidget {
       home: const HomePage(),
       theme: AppTheme().theme(),
       routes: {
-        "/chat": (context)=>ChatScreen()
+        "/chat": (context)=> ChatScreen(),
+        "/new_chat": (context)=>const NewChat(),
+        "/profile": (context)=>const UserProfile(),
       },
     );
   }
